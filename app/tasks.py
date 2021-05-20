d54f546af1cc20578eb2e51dd2f1244d6f9d7ee7
Your Name <you@example.com>
2021-05-20 16:13:54 +0300

    add progress bar

--- app/tasks.py
+++ app/tasks.py
@@ -7,6 +7,8 @@ from time import sleep
 @shared_task(bind=True)
 def go_to_sleep(self, duration):
     progress_recorder = ProgressRecorder(self)
-    sleep(duration)
+    for i in range(5):
+        sleep(duration)
+        progress_recorder.set_progress(i+1, 5, f'On iteratin {i}')
     return 'Done'
 
