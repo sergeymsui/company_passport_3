4b39df87c454d1e1a01ddb0c1c9092ca84339278
Your Name <you@example.com>
2021-05-29 19:16:50 +0300

    Дописаны коментарии

--- app/tasks.py
+++ app/tasks.py
@@ -4,6 +4,7 @@ from celery_progress.backend import ProgressRecorder
 
 from time import sleep 
 
+# Тестовая задача
 @shared_task(bind=True)
 def go_to_sleep(self, duration):
     progress_recorder = ProgressRecorder(self)
@@ -14,11 +15,15 @@ def go_to_sleep(self, duration):
 
 from .domain import Parser
 
+# Конвертация паспорта
 @shared_task(bind=True)
 def go_to_parse(self, duration, t_path):
+    # ProgressRecorder для отслеживания статуса процесса
     progress_recorder = ProgressRecorder(self)
 
+    # Парсер паспорта
     p = Parser()
+    # Запуск конвертации
     p.parse(t_path)
     for i in range(5):
         sleep(duration)
